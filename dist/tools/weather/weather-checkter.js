2c32dd79810ef9584d8282660b10c844f3d545ae
toykam <kabirtoyyib19@gmail.com>
2026-01-07 17:41:57 +0100

    initial commit: McpServer featuring various tools (currency, unit and time convertion, weather data and calculator for basic operations

--- dist/tools/weather/weather-checkter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class WeatherChecker {
-}
-const weatherChecker = new WeatherChecker();
-exports.default = weatherChecker;
